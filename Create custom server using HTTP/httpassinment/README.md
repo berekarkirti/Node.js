e6707eb3afc70bb64623efbc24c8c88a9ef4974d
kirti berekar <berekarkirti11@gmail.com>
2024-10-05 08:52:46 +0530

    task done

---
+++ Create custom server using HTTP/httpassinment/README.md
@@ -0,0 +1,3 @@
+My Video URL :- ""
+![alt text](<Screenshot 2024-10-05 083150.png>)  ![alt text](<Screenshot 2024-10-05 083222.png>)
+![alt text](<Screenshot 2024-10-05 084119.png>)  ![alt text](<Screenshot 2024-10-05 084144.png>)
---
+++ Create custom server using HTTP/httpassinment/index.js
@@ -0,0 +1,51 @@
+const http = require("http");
+const fs = require("fs")
+
+const server = http.createServer((req, res) => {
+    console.log(req.method, req.url)
+
+    if (req.method == "GET" && req.url == "/home") {
+        res.end("<h1>Welcome</h1>")
+    }
+    else if (req.method == "GET" && req.url == "/about") {
+        res.end("<h1>This is About</h1>")
+    }
+    else if (req.method == "GET" && req.url == "/getproductdata") {
+        fs.readFile("./db.json", "utf-8", (err, products) => {
+            if (err) 
+            {
+                console.log(err)
+                res.end(err)
+            }
+            else 
+            {
+                const productdatafromdb = JSON.parse(products)
+                console.log(products)
+                res.end(JSON.stringify(productdatafromdb.products))
+            }
+        })
+    }
+    else if (req.method == "GET" && req.url == "/user") {
+        fs.readFile("./db.json", "utf-8", (err, user) => {
+            if (err) 
+            {
+                console.log(err)
+                res.end(err)
+            }
+            else {
+                const userdatafromdb = JSON.parse(user)
+                console.log(user)
+                res.end(JSON.stringify(userdatafromdb.user))
+            }
+        })
+    }
+    else {
+        res.end("<h1>404 Not Found</h1>")
+    }
+})
+
+server.listen(8080, () => {
+    console.log("server is running at 8080")
+})
+
+// http://localhost:8080
