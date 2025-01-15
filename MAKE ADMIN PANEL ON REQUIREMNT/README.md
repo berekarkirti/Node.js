47dda0f8bc64334a8fd5730a33ad5925dbd4f029
kirti berekar <berekarkirti11@gmail.com>
2025-01-15 16:34:33 +0530

    task done

---
+++ MAKE ADMIN PANEL ON REQUIREMNT/README.md
@@ -0,0 +1,3 @@
+My Video URL:- ""
+
+Output:- "https://1drv.ms/f/s!ArKCxp109EfSjZMsc6ziLowW7rREwQ?e=K8E7nm"
---
+++ MAKE ADMIN PANEL ON REQUIREMNT/middleware/logger.js
@@ -0,0 +1,8 @@
+function logger(req, res, next) 
+{
+    const timestamp = new Date().toString();
+    console.log(`URL: ${req.url}, Method: ${req.method}, Timestamp: ${timestamp}`);
+    next();
+}
+
+module.exports = logger;
